229cd544db6ed48f4d72a261ee53e2b9be0962d2
Ján Korček <korcekdevelopment@gmail.com>
2023-03-10 09:05:48 +0100

    feat: add icons

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
+import { VueQueryPlugin as vueQuery } from 'vue-query';
 import router from '@/router';
 
 import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
@@ -9,6 +11,9 @@ import * as directives from 'vuetify/directives';
 import App from '@/App.vue';
 
 const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'dark',
+  },
   components,
   directives,
 });
@@ -16,5 +21,6 @@ const vuetify = createVuetify({
 const app = createApp(App);
 
 app.use(router);
+app.use(vueQuery);
 app.use(vuetify);
 app.mount('#app');
