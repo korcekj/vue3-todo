096f24b1d15fcc0caad64c79e30db318ef7b2a90
Ján Korček <korcekdevelopment@gmail.com>
2023-03-10 09:05:29 +0100

    feat: add app bar

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
-  <router-view />
+  <v-layout>
+    <v-app-bar :elevation="0">
+      <template v-slot:prepend>
+        <v-icon size="large" icon="mdi-check-all"></v-icon>
+      </template>
+
+      <v-app-bar-title
+        ><v-btn to="/"
+          >Vue3 <span class="font-weight-black">ToDo</span></v-btn
+        ></v-app-bar-title
+      >
+
+      <template v-slot:append> </template>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-layout>
 </template>
