93a01035038b1b1151e63e7e257c2eeaed588fbe
Ján Korček <korcekdevelopment@gmail.com>
2023-03-10 19:59:08 +0100

    feat: add todo filter types

--- src/utils/zod.ts
+++ src/utils/zod.ts
@@ -14,5 +14,8 @@ export const TodoListSchema = z.object({
   todos: z.array(TodoSchema),
 });
 
+export const TodoFilterSchema = z.enum(['all', 'active', 'done']);
+
 export type ITodo = z.infer<typeof TodoSchema>;
 export type ITodoList = z.infer<typeof TodoListSchema>;
+export type ITodoFilter = z.infer<typeof TodoFilterSchema>;
